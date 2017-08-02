eeb086c97f39167bec65cb01e016677640388945
Cassio Moreto <cassio.moreto@fluig.com>
2017-08-02 11:00:54 -0300

    Checking server status using netcat

---
+++ netcat.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+netcatCommand(){
+	RESULT="";
+	nc -z -w 1 $1 $2
+	RESULT=$(echo $?)
+}
+
+check(){
+	netcatCommand $HOST $PORT
+	if [[ $RESULT == 0 ]]; then
+		echo "Server is UP"
+	else
+		echo "Server is DOWN"
+	fi
+}
+
+helpMsg(){
+	echo ""
+	echo "Usage:"
+	echo "  netcat.sh -H[destination] -L[port]"
+	echo ""
+	echo "		-H : destination host that will check"
+	echo "		-L : destination host port that will check"
+	exit 3;
+}
+
+if [[ "$#" -lt 1 ]]; then
+	echo "Required args is missing";
+	echo $#;
+	exit 1;
+fi
+
+while getopts 'H:L:F' OPT; do
+   case $OPT in
+      H) HOST=$OPTARG;;
+      L) PORT=$OPTARG;;
+			F) helpMsg;;
+      *) exit 1;;
+   esac
+done
+
+check
